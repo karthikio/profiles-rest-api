a92dcb5d63cd13e018b65904727bcec518617640
karthik santhosh <developer.karthiksanthosh@gmail.com>
2021-06-07 19:32:38 +0530

    Added ApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+
+    def get(self, request, format=None):
+        new_list = [
+            'I\'m Karthik Santhosh',
+            'Full Stack Web Developer',
+            'Freelancer',
+            'Programmer',
+        ]
+
+        return Response({'mess': 'Hello World', 'new_list': new_list})

f54b755d153c1eedffce3b4f1ea28db3d94ce0d4
karthik santhosh <developer.karthiksanthosh@gmail.com>
2021-06-08 18:40:12 +0530

    Added Login Api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,7 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets, filters
 from rest_framework.authentication import TokenAuthentication
-
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from . import permissions
 from . import models
@@ -80,3 +81,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email')
+
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
